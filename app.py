f048a13497f203c1e9367a2506fdff8ed9082051
Moritz Geiger <moritzgeiger@hotmail.de>
2021-05-01 20:29:59 +0200

    finish notebook, init heroku

---
+++ app.py
@@ -0,0 +1,16 @@
+# TABLES & VIS
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from ast import literal_eval
+import numpy as np
+
+# ML
+from sklearn.neighbors import KNeighborsRegressor
+from joblib import dump, load
+
+# PRODUCTION
+import streamlit as st
+
+
+
